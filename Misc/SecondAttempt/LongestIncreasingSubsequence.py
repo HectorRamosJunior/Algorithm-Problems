c1af9bb5ac276f66855e156386eb73b9cb922e02
Hector Ramos <HectorRamos.Junior@gmail.com>
2016-04-03 20:18:32 -0400

    Slight optimization printing longest subsequence

--- Misc/SecondAttempt/LongestIncreasingSubsequence.py
+++ Misc/SecondAttempt/LongestIncreasingSubsequence.py
@@ -15,6 +15,8 @@ def longest_increasing_subsequence(array):
     # at each array index possible for the given index's value.
     lis_list = [ [] for x in xrange(len(array)) ]
 
+    longest_subsequence = []
+
     # Goes through creating a longest_increasing_subsequence_list for
     # Each element in the array; this guarantees all longest subsequences
     for i in xrange(len(array)):
@@ -29,12 +31,14 @@ def longest_increasing_subsequence(array):
         # Append the value of i to the end of the lis for this index
         lis_list[i].append(array[i])
 
-    # Return the longest subsequence in the array of longest subsequences
-    # For each possible element in the array
-    longest_subsequence = []
-    for subsequence in lis_list:
-        if len(subsequence) > len(longest_subsequence):
-            longest_subsequence = subsequence
+        # Keep track of the longest subsequence so far
+        if len(lis_list[i]) > longest_subsequence:
+            longest_subsequence = lis_list[i]
+
+        # For debugging purposes.
+        #print "The longest subsequence for index %d:" % i
+        #print lis_list[i]
+
 
     return longest_subsequence
 
